ec309eb2c999dbd653d6d836b090b44b979c4b41
Anzar Salam <anzarsalamr@gmail.com>
2022-07-04 23:51:27 +0530

    Add Routing in the Application

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Introduction from './components/Landing/Introduction';
+import UnderConstruction from './components/Sorry/UnderConstruction';
+import About from './pages/about';
+import Contact from './pages/contact';
+import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Routes>
+          <Route path="/" element={<UnderConstruction />} />
+          <Route path="home" element={<Home />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="about" element={<About />} />
+          <Route path="intro" element={<Introduction />} />
+        </Routes>
       </header>
     </div>
   );
