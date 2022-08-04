1ee1ad0256499bf2da82ce59810a118e0b4d5146
Anzar <anzar@qburst.com>
2022-08-04 22:59:20 +0530

    Navbar added with Branding.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Introduction from './components/Landing/Introduction';
-import UnderConstruction from './components/Sorry/UnderConstruction';
+import Nav from './components/Navbar/Nav';
 import About from './pages/about';
 import Contact from './pages/contact';
 import Home from './pages/home';
 
 function App() {
   return (
+    <>
+    <Nav/>
     <div className="App">
       <header className="App-header">
         <Routes>
-          <Route path="/" element={<UnderConstruction />} />
-          <Route path="home" element={<Home />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="about" element={<About />} />
-          <Route path="intro" element={<Introduction />} />
+            <Route path="/" element={<Introduction />} />
+            <Route path="home" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
         </Routes>
       </header>
     </div>
+    </>
   );
 }
 
